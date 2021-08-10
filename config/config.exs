bcf09fa9b9a08b185985ed18dc840182373b43e6
Мизеров Егор Викторович <mizerov.e@myseldon.com>
2021-08-11 00:45:36 +0500

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,36 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :blog,
+  ecto_repos: [Blog.Repo],
+  adapter:  Ecto.Adapters.Postgres,
+  database: "dev",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
+# Configures the endpoint
+config :blog, BlogWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "VfQV7g47TdsNGVh9K5NmObCVC2xHUtNhe9uc+vLwNqy1b0H4yzsMIA3X5ZLrSd0x",
+  render_errors: [view: BlogWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Blog.PubSub,
+  live_view: [signing_salt: "cdRc8ZUU"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
